1c17df0d42abc0dc746297f1b58a4e008b3f4398
jw7914 <jw7914@nyu.edu>
2024-09-22 22:25:25 -0400

    Initial Commit

---
+++ Project1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Jason Wu on 9/22/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
